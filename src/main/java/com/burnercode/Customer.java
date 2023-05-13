3fb8aa43b7cf6a2033d70d7ca3f56dbca8d87c5c
samratbatra <samratbatra8@gmail.com>
2023-05-13 23:22:48 +0530

    First commit

---
+++ src/main/java/com/burnercode/Customer.java
@@ -0,0 +1,22 @@
+package com.burnercode;
+
+public class Customer {
+
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String age;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String name, String email, String age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/burnercode/Main.java
@@ -0,0 +1,36 @@
+package com.burnercode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String greet() {
+        return "Hello";
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greetResponse() {
+        return new GreetResponse("Hello", List.of("C","C++","Java"),new Person("Samrat", 28, 30000));
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person) {
+    };
+    record Person(String name, int age, double savings) {
+    };
+}
+
+
